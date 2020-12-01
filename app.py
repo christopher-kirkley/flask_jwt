a0dc87795e8382420639f5e1742e4ee6899af79c
Christopher Kirkley <ckirkley@gmail.com>
2020-12-01 08:29:39 -0800

    Add tests, set up api.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, jsonify
 import jwt
 import datetime
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +16,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50))
     name = db.Column(db.String(50))
     password = db.Column(db.String(50))
     admin = db.Column(db.Boolean)
@@ -22,44 +25,98 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
-    useer_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.args.get('token')
-        if not token:
-            return jsonify({'message': 'Token is missing'}), 403
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return jsonify({'message': 'Token is invalid'}), 403
-        return f(*args, **kwargs)
-    return decorated
+@app.route('/user', methods=['GET'])
+def get_all_users():
 
-@app.route('/unprotected')
-def unprotected():
-    return jsonify({'message': 'Anyone can view this.'})
+    users = User.query.all()
 
-@app.route('/protected')
-@token_required
-def protected():
-    return jsonify({'message': 'This is only available for people with valid tokens'})
+    output = []
+
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+
+    return jsonify({'users': output})
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found'})
+
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsoniify({'user': user_data})
+    
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json(force=True)
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'New user created'})
+
+@app.route('/user/<public_id>', methods=['PUT'])
+def promote_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found'})
+    
+    user.admin = True
+    db.session.commit()
+
+    return jsonify({'message': 'User promoted'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
+def delete_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found'})
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User deleted'})
 
 @app.route('/login')
 def login():
     auth = request.authorization
-   
-    if auth and auth.password == 'secret':
-        token = jwt.encode({
-            'user': auth.username,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
-            },
-            app.config['SECRET_KEY']
-            )
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401)
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login!"'})
+
+    if check_password_has(user.password, auth.password):
+        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
         return jsonify({'token': token.decode('UTF-8')})
 
-    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login!"'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tests.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def test_return_status_code():
+    response = requests.get("http://localhost:5000/user")
+    assert response.status_code == 200
+
+def test_can_post_new_user():
+    payload = {
+            'password': 'pineapple',
+            'name': 'stevie b'
+            }
+    response = requests.post("http://localhost:5000/user", data=json.dumps(payload))
+    assert response.status_code == 200
+    assert json.loads(response.content) == {'message': 'New user created'}
+
+def test_can_get_all_users():
+    response = requests.get("http://localhost:5000/user")
+    assert response.status_code == 200
+    assert len(response.content) > 0
+
+def test_can_return_one_user():
+    response = requests.get("http://localhost:5000/user/1")
+    assert response.status_code == 200
+
+def test_can_delete_user():
+    public_id = 'f87f29c8-9843-4e34-9c68-1bd6449c7c5e'
+    response = requests.delete(f"http://localhost:5000/user/{public_id}")
+    assert response.status_code == 200
+
+def test_can_promote_one_user():
+    public_id = 'f87f29c8-9843-4e34-9c68-1bd6449c7c5e'
+    response = requests.put(f"http://localhost:5000/user/{public_id}")
+    assert response.status_code == 200
+    assert json.loads(response.content) == {'message': 'User promoted'}
